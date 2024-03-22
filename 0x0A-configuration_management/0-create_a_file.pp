49dc92bf2080ffb8df2a6970299267b249340300
BekiHabesha <bereketdere@gmail.com>
2024-03-22 13:04:34 -0700

    No0.Create a file.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This code creates a file name school inside the /tmp Directory, Using Puppet.
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
