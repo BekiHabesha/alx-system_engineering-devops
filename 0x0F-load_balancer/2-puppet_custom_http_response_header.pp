5554cc4fe33314114e7ccf5273ebcfa88b45b1d4
BekiHabesha <bereketdere@gmail.com>
2024-04-01 15:21:38 -0700

    No2.Add a custom HTTP header with Puppet.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# It Installs a Nginx server with a custom HTTP header "X-Served-By" response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
