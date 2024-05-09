8bde7c89fd0742dc9de854c1c181eac294f7a3cd
BekiHabesha <bereketdere@gmail.com>
2024-05-09 14:50:38 -0700

    No.0.How many subs.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,19 @@
 """ Module for task 0 """
 import requests
 
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Bekihabesha"}
+    response = requests.get(url, allow_redirects=False)
+    
+    if response.status_code != 200:
+         return None
 
-def number_of_subscribers(subreddit, hot_list=[], after="", count=0):
-    """ Queries the Reddit API of the users"""
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "bekihabesha"},
-    )
+    data = requests.json()
+    subscribers = data['data']['subscribers']
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers", 0)
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     else:
         return 0
