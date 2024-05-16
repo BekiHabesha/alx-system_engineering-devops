8ce3e5b737d83d49c197a9f707f5aedb465786b0
BekiHabesha <bereketdere@gmail.com>
2024-05-16 16:57:25 +0000

    No. 1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,2 @@
-# Enable the user holberton to login and open files without error.
-
-# Increase hard file limit for Holberton user.
-exec {'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-}
-
-# Increase soft file limit for Holberton user.
-exec {'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-}
+# pupet script
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
